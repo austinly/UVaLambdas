03229f691b37d8f36000e5b32471b6fa2dfe56a1
min <mjkleben@gmail.com>
2019-01-11 20:18:45 -0500

    fixed header/footer path issue

--- templates/header.php
+++ templates/header.php
@@ -1,7 +1,7 @@
 <html>
     <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" type="text/css" href="/LFESite/static/header.css">
+        <link rel="stylesheet" type="text/css" href="../static/header.css">
         <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
         <link href="https://fonts.googleapis.com/css?family=News+Cycle" rel="stylesheet">
         <script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
